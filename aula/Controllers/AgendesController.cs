2631adf75a9049a3c2505bed2979dbc9276db3a2
viitto <viito.nascimento@gmail.com>
2022-11-11 15:10:05 -0300

    css agende

---
+++ aula/Controllers/AgendesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aula.Controllers
+{
+    public class AgendesController : Controller
+    {
+        // GET: Agendes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
