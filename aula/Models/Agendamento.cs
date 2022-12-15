af75aaa34241d79514c2e5973ccb66d671756492
Luis Gustavo Pereira Nunes <g.luis@academico.ifrn.edu.br>
2022-12-15 11:38:22 -0300

    começando o tratamento de erro

--- aula/Models/Agendamento.cs
+++ aula/Models/Agendamento.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace aula.Models
 {
     public class Agendamento
     {
         public long AgendamentoId { get; set; }
-        public string Data{ get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Cadastro")]
+        [Required(ErrorMessage = "Informe a data de cadastro do produto")]
+        public DateTime Data{ get; set; }
         public long? ClienteId { get; set; }
         public long? ServiçoId { get; set; }
         public long? FuncionarioId { get; set; }
+        public string Horario { get; set; }
         public Cliente Cliente { get; set; }
         public Serviço Serviço { get; set; }
         public Funcionario Funcionario { get; set; }
